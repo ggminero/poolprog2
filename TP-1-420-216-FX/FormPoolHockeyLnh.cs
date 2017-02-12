97b7178be2e335c4b40ec5feecd7b5923619fc1f
Louis Villeneuve <villeneuve25.louis@gmail.com>
2017-02-11 23:20:40 -0500

    ajout des classes manquantes (qui étaient supposées etre dans mon dernier commit)

---
+++ TP-1-420-216-FX/FormPoolHockeyLnh.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_1_420_216_FX
+{
+    /// <summary>
+    /// Classe pour le formulaire principal du pool de hockey
+    /// </summary>
+    public class FormPoolHockeyLnh
+    {
+        #region ATTRIBUTS
+
+        /// <summary>
+        /// Le pool de hockey géré par le formulaire
+        /// </summary>
+        private PoolHockeyLnh _pool;
+
+        #endregion
+
+    }
+}
