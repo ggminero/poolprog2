3bc6b066e007cb3c1af00973ca9fe7e78017a25d
Louis Villeneuve <villeneuve25.louis@gmail.com>
2017-02-14 22:41:39 -0500

    continuation de la classe Utilitaire.cs

--- TP-1-420-216-FX/Utilitaire.cs
+++ TP-1-420-216-FX/Utilitaire.cs
@@ -104,9 +104,42 @@ namespace TP_1_420_216_FX
             fluxLectPart.Close();
 
             // On retourne le vecteur de joueurs créé.
-            return vectPart;
+            return vectPart;          
+        }
 
-            #endregion
+        /// <summary>
+        /// Permet d'enregistrer les données sur les participants dans un fichier
+        /// texte(sérialisation). --> CLASSE PARTICIPANT À CRÉER
+        /// </summary>
+        public static void EnregistrerParticipants(string cheminFichier, Participant[] vectParticipants)
+        {
+            // Création du flux d'écriture dont le chemin d'accès est "cheminFichier"
+            StreamWriter fluxEnregistre = new StreamWriter(cheminFichier, false);
+
+            // Chaîne de caractères pour la version sérialisé d'un objet "Participant"
+            string participantTexte;
+
+            // Traitement de chaque objet du vecteur
+            for (int i = 0; i < vectParticipants.Length; i++)
+            {
+                // Création de la version sérialisée de l'objet "Participant"
+                participantTexte = vectParticipants[i].Prenom + ", ";
+
+                // Écriture de la version sérialisée de l'objet dans un fichier
+                fluxEnregistre.WriteLine(participantTexte);
+            }
+
+            // Fermeture du flux vers le fichier
+            fluxEnregistre.Close();
+        }
+
+        /// <summary>
+        /// Permet de formater pour l'affichage
+        /// une chaîne de caractères en format casse Pascal ou casse mixte.
+        /// </summary>
+        public static string FormaterChainePascalOuMixte(string chaine)
+        {
+            
         }
     }
 }
