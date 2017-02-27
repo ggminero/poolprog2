2f8196426bfb2e9a4eec7ba14c4cff16b2586e77
ggminero <g.minero@edu.cegepgarneau.ca>
2017-02-26 22:30:20 -0500

    Add files via upload
    missing formfichejoueur

--- TP-1-420-216-FX/FormFicheJoueur.Designer.cs
+++ TP-1-420-216-FX/FormFicheJoueur.Designer.cs
@@ -167,12 +167,14 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(25, 322);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 11;
             this.button1.Text = "Fermer";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.frmFicheJoueurQuitter_Quitter);
             // 
             // pictureBoxJoueur
             // 
